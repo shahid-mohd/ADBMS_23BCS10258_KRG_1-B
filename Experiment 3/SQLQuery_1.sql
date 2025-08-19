6355c6d2c6a1255ac775b71aa057518c5da1e995
Mohd Shahid <155215858+shahid-mohd@users.noreply.github.com>
2025-08-19 22:42:32 +0530

    Create SQLQuery_1.sql

---
+++ Experiment 3/SQLQuery_1.sql
@@ -0,0 +1,34 @@
+CREATE TABLE Department (
+    ID INT PRIMARY KEY,
+    DEPT_NAME VARCHAR(50)
+);
+
+CREATE TABLE Employee (
+    ID INT,
+    NAME VARCHAR(50),
+    SALARY INT,
+    DEPT_ID INT,
+    FOREIGN KEY (DEPT_ID) REFERENCES Department(ID)
+);
+
+INSERT INTO Department (ID, DEPT_NAME) VALUES
+(1, 'IT'),
+(2, 'SALES');
+
+INSERT INTO Employee (ID, NAME, SALARY, DEPT_ID) VALUES
+(1, 'JOE', 70000, 1),
+(2, 'JIM', 90000, 1),
+(3, 'HENRY', 80000, 2),
+(4, 'SAM', 60000, 2),
+(5, 'MAX', 90000, 1);
+
+SELECT d.DEPT_NAME, e.NAME, e.SALARY FROM Department as d
+INNER JOIN Employee as e
+ON d.id = e.DEPT_ID
+WHERE e.SALARY IN 
+(
+  SELECT MAX(E2.SALARY) 
+  FROM Employee as E2 
+  where E2.DEPT_ID = e.DEPT_ID
+)
+ORDER by d.DEPT_NAME
